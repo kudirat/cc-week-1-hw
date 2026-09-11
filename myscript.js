314d75369db3e4e128522949e95d320f60419a07
Kudirat Alimi <47795311+kudirat@users.noreply.github.com>
2026-09-11 12:26:11 -0400

    Initial commit

---
+++ myscript.js
@@ -0,0 +1,3 @@
+const colors = ["#b5acee", "#9f5896", "#6c87ff"];
+const entryDate = document.querySelector("#entry-1-date");
+entryDate.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
