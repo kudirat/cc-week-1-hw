eaa62fe785c0f702c1013b9fcb4f3817cd573be2
Kudirat Alimi <47795311+kudirat@users.noreply.github.com>
2026-09-17 21:29:52 -0400

    -styled navigation buttons -made it so that nav buttons background matches the entry title background when randomized

--- myscript.js
+++ myscript.js
@@ -1,3 +1,7 @@
 const colors = ["#b5acee", "#9f5896", "#6c87ff"];
 const entryDate = document.querySelector("#entry-1-date");
 entryDate.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+const prevBtn = document.querySelector("#entry-1-prev-btn");
+const nextBtn = document.querySelector("#entry-1-next-btn");
+const navBtns = document.querySelector("#entry-1-nav-btns");
+navBtns.style.backgroundColor = entryDate.style.backgroundColor;
